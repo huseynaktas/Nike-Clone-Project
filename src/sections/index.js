542c0d887d6fc364fef5f1a8c9fdc185e904fe09
huseynaktas <h.akts1007@gmail.com>
2024-03-27 12:47:15 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProdact from './PopularProduct';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export{
+    Hero,
+    PopularProdact,
+    SuperQuality,
+    SpecialOffer,
+    Services,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
